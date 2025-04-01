0907475ab7b6911df9ed76853b8f45cc32b0d64c
Ifeanyi Chima <chimaifeanyi29@gmail.com>
2025-04-01 08:29:50 -0700

    feat: finish implementing body styling

--- src/features/coins/Coins.jsx
+++ src/features/coins/Coins.jsx
@@ -1,23 +1,21 @@
 import { useState } from 'react';
 import { useGetCoinsQuery } from './coinsApiSlice';
-import TableData from './TableData';
-import { FaArrowRight, FaArrowLeft, FaSearch } from "react-icons/fa";
 import { useGetCoinQuery } from "../coinSearch/coinSearchApiSlice";
 import useDebounce from '../../hooks/useDebounce';
 import Header from '../../components/Header/Header';
 import CoinTable from './CoinTable';
-import Change from './Change';
 import NavArrows from '../../components/NavArrows/NavArrows';
 import SearchBox from '../../components/SearchBox/SearchBox';
+import "./coins_css/Coins.css"
+
 
 const Coins = () => {
 
-    const returnPrice = (price) => {
-        return `\u0024 ${Number(price).toLocaleString("en-US")}`;
-    }
-
     // track the current page
-    const [page, setPage] = useState(1);
+    const [currentPage, setCurrentPage] = useState(1); 
+
+    // const [page, setPage] = useState(1); // track the page number
+
 
     // track user search request
     const [search, setSearch] = useState("");
@@ -25,14 +23,16 @@ const Coins = () => {
     // make API call 500ms after user stops typing
     const debouncedSearchQuery = useDebounce(search, 500);
 
+    // API query parameters
     const queryRequest = {
         vs_currency: "usd", // display price in USD
         order: "market_cap_desc", // coins by marketcap in descending order
         per_page: "10", // number of coins per page
         sparkline: "false",
-        page
+        page: currentPage // Current Page
     }
 
+    // search for a particular coin
     const coinQueryRequest = {
         vs_currency: "usd", // display price in USD 
         order: "market_cap_desc", // coins by marketcap in descending order
@@ -43,7 +43,7 @@ const Coins = () => {
     }
 
     const {
-        data: coins, // all the coins that was fetched or recieved
+        data: coins, // all the coins that was fetched
         isSuccess,
         isError,
         error,
@@ -55,34 +55,23 @@ const Coins = () => {
 
 
     const handlePrevClick = async () => {
-        try {
-            setPage(prev => parseInt(prev - 1));
-        } catch (e) {
-            console.log(e.message);
-        }
+        setCurrentPage(prev => parseInt(prev - 1));
     }
 
     const handleNextClick = async () => {
-        try {
-            setPage(prev => parseInt(prev + 1));
-        } catch (e) {
-            console.log(e.message);
-        }
+        setCurrentPage(prev => parseInt(prev + 1));
     }
 
     const handleSubmit = (e) => e.preventDefault();
 
     const handleSearch = (e) => {
-        let lowerCase = e.target.value;
-        setSearch(lowerCase)
+        setSearch(e.target.value)
     }
 
-    // <div className="row">
-    //     <div className="col-12 intro text-center">
-    //         <h3 className="mt-4">Coins</h3>
-    //         <p className="lead">Every crypto in the world on your screen</p>
-    //     </div>
-    // </div>
+    // Handle dot click to navigate pages
+    // const handleDotClick = (pageNumber) => {
+    //     setCurrentPage(pageNumber); // Update the current page
+    // };
 
   return ( 
     <>
@@ -90,93 +79,87 @@ const Coins = () => {
         
         <div className="coin-page">
 
+            <main className="container d-flex flex-column flex-grow-1">
+              
+                <div className="">
 
-            <main className="container">
-                
-                {/* <div className="sticky top-0 z-3">
-                </div> */}
-                
-
-                <SearchBox handleSearch={handleSearch} handleSubmit={handleSubmit} search={search}/>
+                    <SearchBox handleSearch={handleSearch} handleSubmit={handleSubmit} search={search}/>
 
                 
-                <NavArrows page={page} handleNextClick={handleNextClick} handlePrevClick={handlePrevClick} />
-                
-                
-                
-                <div className="row">
-                    
-                    {isLoading && <div className="mexican-wave text-center my-5 white-loading"></div>}
-                        
-                    {isError && (<p data-testid="error" className="text-center text-danger">Oh no, there was an error {JSON.stringify(error.error)} </p>)}
-                        
-                    {/* {isError && (<p data-testid="error" className="text-center text-danger">{error.data.message}</p>)} */}
+                    <NavArrows page={currentPage} handleNextClick={handleNextClick} handlePrevClick={handlePrevClick} />
 
-                    {
-                        isSuccess && (
-                            <div className='table-responsive cointable_overflow' style={{ height: "100vh"}}>
-                                <table className="table table-hover">
-                                    <thead>
-                                        <tr className='border-bottom'>
-                                            <th className="py-3 cursor-pointer">Name</th>
-                                            <th className="py-3 cursor-pointer text-end">Price</th>
-                                        </tr>
-                                    </thead>
+                    <div className="page-indicator">
+                    {Array.from({ length: 2 }).map((_, index) => {
 
-                                    <tbody className="bg-white">
-                                        {!searchedForCoin && coins.map((each, i) => (
-                                        <CoinTable key={i} each={each}/> 
-                                        ))}
+                        // Calculate the page number based on the index and currentPage
+                        // Calculate the page number for each dot
+                        // const pageNumber = Math.max(1, Math.min(10, currentPage - 1 + index));
 
-                                        {searchedForCoin && coinSearchResult.map((each, i) => (
-                                            <CoinTable key={i} each={each} />
-                                        ))}
-                                    </tbody>
-                                </table>
-                            </div>
+
+                        // Calculate the page number for each dot
+                        const pageNumber = currentPage + index;
+
+
+                        // Ensure the page number doesn't exceed the maximum (10)
+                        if (pageNumber > 10) return null;
+
+
+                        return (
+                            <div
+                                key={index}
+                                className={`page-dot ${pageNumber === currentPage ? "active" : ""}`}                               
+                            />
                         )
-                    }    
-
-                        
-                    {/* {isSuccess && (
-                        <div 
-                        
-                            // className="table_responsive"
-                            className="table-container"
-                            style={{
-                                height: "calc(100vh - 100px)", // Adjust height to fill the screen minus the header height
-                                overflowY: "auto", // Enable vertical scrolling
-                                overflowX: "auto", // Enable horizontal scrolling
-                                // padding: "1rem",
-                                boxSizing: "border-box",
-                            }}
-                        >
-                            <table 
-                                // className="table tableDesign"
-                                className="table table-striped"
-                                style={{
-                                width: "100%",
-                                borderCollapse: "collapse",
-                                tableLayout: "fixed", // Ensures consistent column widths
-                                }}>
-                                
+                    })}
+                    </div>
+                
+                
+                
+                    
 
 
-                                <tbody className="bg-white" id="tbody">
-                                    {!searchedForCoin && coins.map((each, i) => (
-                                        <CoinTable key={i} each={each} />
-                                    ))}
 
-                                    {searchedForCoin && coinSearchResult.map((each, i) => (
-                                        <CoinTable key={i} each={each} />
-                                    ))}
-                                </tbody>
-                            </table>
+                    {isLoading && <div className="mexican-wave text-center my-5 white-loading"></div>}
+                    
+                    
+                    {searchedForCoin && coinSearchResult.length === 0 && (
+                        <div>
+                            <p data-testid="coinError" className="text-center font-weight text-danger">Oh no, coin not found</p>
                         </div>
-                    )}  */}
+                    )}
 
-                    {searchedForCoin && coinSearchResult.length === 0 && (<p data-testid="coinError" className="text-center font-weight text-danger">Oh no, coin not found</p>)}
-                                
+
+                    {isError && (
+                        <div className='d-flex flex-grow-1 justify-content-center align-items-center' style={{ height: "100%" }}>
+                            <p data-testid="error" className="text-center text-danger">Oh no, there was an error {JSON.stringify(error.error)} </p>
+                        </div>
+                    )}
+
+
+                    {isSuccess && (
+                    <div className='table-responsive cointable_overflow' style={{ height: "100vh"}}>
+                        <table className="table table-hover">
+                            <thead>
+                                <tr className='border-bottom'>
+                                    <th className="py-3 cursor-pointer">Name</th>
+                                    <th className="py-3 cursor-pointer text-end">Price</th>
+                                </tr>
+                            </thead>
+
+                            <tbody className="bg-white">
+                                {!searchedForCoin && coins.map((each, i) => (
+                                <CoinTable key={i} each={each}/> 
+                                ))}
+
+                                {searchedForCoin && coinSearchResult.map((each, i) => (
+                                    <CoinTable key={i} each={each} />
+                                ))}
+
+                            </tbody>
+                        </table>
+                    </div>
+                    )}                                    
+                
                 </div>
             </main>
         </div>
