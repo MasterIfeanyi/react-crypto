d4620d56ea3dd4ef76bf2ecb2e14d094f5aa44b0
Ifeanyi Chima <chimaifeanyi29@gmail.com>
2025-04-04 06:06:17 -0700

    fix: Netlify build error

--- src/app/store.js
+++ src/app/store.js
@@ -2,12 +2,6 @@ import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import {
   persistReducer,
   persistStore,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
 } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 import { apiSlice } from "./api/apiSlice";
